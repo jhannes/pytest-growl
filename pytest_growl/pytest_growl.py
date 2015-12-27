2bbcad2c1de2d5163afeb3f9a7c0643dfb510de6
Johannes Brodwall <johannes@brodwall.com>
2015-12-27 13:13:58 +0100

    Handle all tests skipped. Display error details when only one test fails

--- pytest_growl/pytest_growl.py
+++ pytest_growl/pytest_growl.py
@@ -1,5 +1,6 @@
 import time
 import pkg_resources
+import os
 from growl import growl
 
 icon_success = pkg_resources.resource_stream(__name__, "icon_success.png").read()
@@ -30,33 +31,57 @@ def pytest_terminal_summary(terminalreporter):
     if terminalreporter.config.option.growl:
         tr = terminalreporter
         quiet_mode = tr.config.getini(QUIET_MODE_INI)
- 
+
         passes = len(tr.stats.get("passed", []) )
         fails = len(tr.stats.get("failed", []) )
         errors = len(tr.stats.get("error", []) )
         skips = len(tr.stats.get("deselected", []) )
 
         if errors == 1:
-            test_name = tr.stats["error"][0].nodeid
-            send_growl(
-                title = "Error in %s" % test_name,
-                message = "Error %s\n(Tests: %d failed, %d ok)" % (test_name, fails, passes),
-                icon = icon_error)
+            error_test = tr.stats["error"][0]
+            try:
+                filename = os.path.basename(error_test.longrepr.reprcrash.path)
+                lineno = error_test.longrepr.reprcrash.lineno
+                message = error_test.longrepr.reprcrash.message
+                send_growl(
+                    title = "1 error %s:%d" % (filename, lineno),
+                    message = "%s\n\n(Tests: %d failed, %d ok)" % (message, fails, passes),
+                    icon = icon_error)
+            except AttributeError:
+                print error_test.longrepr.__dict__
+                send_growl(
+                    title = "1 error %s" % (error_test.nodeid),
+                    message = "%s\n\n%d tests ok" % (error_test.nodeid, passes),
+                    icon = icon_failed)                
         elif errors > 1:
             send_growl(
                 title = "Error in %d files" % errors,
                 message = "Errors: %d\n(Tests: %d failed, %d ok)" % (errors, fails, passes),
                 icon = icon_error)
         elif fails == 1:
-            test_name = tr.stats["failed"][0].nodeid
-            send_growl(
-                title = tr.stats["failed"][0].location[0],
-                message = "%s failed\n%d tests ok" % (test_name, passes),
-                icon = icon_failed)
+            failed_test = tr.stats["failed"][0]
+            try:
+                filename = os.path.basename(failed_test.longrepr.reprcrash.path)
+                lineno = failed_test.longrepr.reprcrash.lineno
+                message = failed_test.longrepr.reprcrash.message
+                send_growl(
+                    title = "1 failed %s:%d" % (filename, lineno),
+                    message = "%s\n\n%d tests ok" % (message, passes),
+                    icon = icon_failed)
+            except AttributeError:
+                send_growl(
+                    title = "1 failed %s" % (failed_test.nodeid),
+                    message = "%s\n\n%d tests ok" % (failed_test.nodeid, passes),
+                    icon = icon_failed)                
         elif fails > 1:
             send_growl(
                 title = "%d tests failed" % fails,
                 message = "%d failed, %d ok" % (fails, passes),
+                icon = icon_failed)
+        elif passes == 0:
+            send_growl(
+                title = "No tests",
+                message = "No tests (%d skipped)" % (skips) ,
                 icon = icon_failed)
         elif skips > 1:
             send_growl(title="Tests Complete",
