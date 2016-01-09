4b5a42b0025aa341c7631e2b5177b34b3f1c12f9
Johannes Brodwall <johannes@brodwall.com>
2016-01-09 20:03:08 +0100

    Include *.png in package_data

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 setup(
-    author="Anthony Long",
-    author_email="antlong@gmail.com",
-    version="0.1",
+    author="Johannes Brodwall",
+    author_email="jhannes@gmail.com",
+    version="0.2",
     description="Growl notifications for pytest results.",
     name="pytest-growl",
+    install_requires=['gntp>=1.0.2'],
+    package_data={'':['*.png']},
     keywords="pytest, pytest-, growl, py.test",
     packages=['pytest_growl'],
     entry_points={'pytest11': ['pytest_growl = pytest_growl.pytest_growl', ]},)
